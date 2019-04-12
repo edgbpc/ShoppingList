ba3c9121b1c95820255ebf79fe216aeb8b91b242
ericgoodwin <edgbpc@mail.umsl.edu>
2019-04-11 23:16:16 -0500

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: ericgoodwin <edgbpc@mail.umsl.edu>
    
    # Conflicts:
    #	app/src/main/java/edu/eric/goodwin/shoppinglist/dialogBoxFragment.kt

--- app/src/main/java/edu/eric/goodwin/shoppinglist/dialogBoxFragment.kt
+++ app/src/main/java/edu/eric/goodwin/shoppinglist/dialogBoxFragment.kt
@@ -14,7 +14,7 @@ class DialogBoxFragment: DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_dialog, container)
 
-
+//
 
         val qtyEditText = view.findViewById(R.id.QtyEditText) as EditText
         val priceEditText = view.findViewById(R.id.priceEditText) as EditText
